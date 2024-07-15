bfbc3330fc666abffa6b78dac9fe5c3b8b7459d3
Zach <zacharyyong_1997@outlook.com>
2024-07-15 22:38:22 +0800

    Initial Commit

---
+++ InfiniteScrollingImageList/InfiniteScrollingImageListApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfiniteScrollingImageListApp.swift
+//  InfiniteScrollingImageList
+//
+//  Created by Zachary on 15/7/24.
+//
+
+import SwiftUI
+
+@main
+struct InfiniteScrollingImageListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
